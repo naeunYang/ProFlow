d4790c79c7aa89449a72ed09752a50f39bf2daff
naeun Yang <didskdms2@naver.com>
2024-04-09 08:03:18 +0900

    first commit

---
+++ src/main/java/org/example/proflow/ProFlowApplication.java
@@ -0,0 +1,13 @@
+package org.example.proflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProFlowApplication.class, args);
+	}
+
+}
